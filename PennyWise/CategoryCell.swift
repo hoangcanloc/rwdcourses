dbcc01d6a3994e77dd484b1b3f05a487676e1a90
Nguyen Hoang <hoangnd17@Nguyens-MacBook-Pro.local>
2020-10-19 10:25:38 +0700

    Initial commit

---
+++ PennyWise/CategoryCell.swift
@@ -0,0 +1,56 @@
+/*
+* Copyright (c) 2016 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+import UIKit
+
+@IBDesignable
+class CategoryCell: UICollectionViewCell {
+  
+  @IBOutlet var imageView: UIImageView!
+  
+  var category: Category? {
+    didSet {
+      imageView.image = category?.image
+    }
+  }
+  
+    override var isSelected:Bool {
+    didSet {
+        if (UIAccessibilityTraitSelected != 0) {
+        imageView.tintColor = appRedColor
+      } else {
+        imageView.tintColor = UIColor.black
+      }
+    }
+  }
+  
+  private func scalePath(path:UIBezierPath, size:CGSize) {
+    // scale path - icons should all be same height
+    let scale = size.height / path.bounds.size.height * 0.5
+    path.apply(CGAffineTransform(scaleX: scale, y: scale))
+    
+    // move path to origin
+    path.apply(CGAffineTransform(translationX: -path.bounds.origin.x, y: -path.bounds.origin.y))
+    // move path into center
+    path.apply(CGAffineTransform(translationX: size.width/2 - path.bounds.width/2, y: size.height/2 - path.bounds.height/2))
+  }
+}
---
+++ PennyWise/NumberView.swift
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2016 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import UIKit
+
+
+@objc protocol NumberViewDelegate {
+    func numberTapped(number:Int)
+}
+
+class NumberView: UIView {
+    
+    @IBOutlet var delegate:NumberViewDelegate?
+    
+    
+    var numberLabel:UILabel?
+    var number: Int = 0 {
+        didSet {
+            numberLabel?.text = "\(number)"
+        }
+    }
+    
+    var label: String? {
+        didSet {
+            numberLabel?.text = label
+        }
+    }
+    
+    @IBAction func handleTap(gesture:UITapGestureRecognizer) {
+        delegate?.numberTapped(number: number)
+    }
+    
+    
+    override func awakeFromNib() {
+        setup()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        setup()
+    }
+    
+    func setup() {
+        numberLabel = UILabel(frame: bounds)
+        guard let numberLabel = numberLabel else { return }
+        numberLabel.textAlignment = .center
+        numberLabel.textColor = darkTextColor
+        numberLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 32)
+        numberLabel.text = "0"
+        addSubview(numberLabel)
+        
+        numberLabel.translatesAutoresizingMaskIntoConstraints  = false
+        numberLabel.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        numberLabel.heightAnchor.constraint(equalTo: heightAnchor).isActive = true
+        numberLabel.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        numberLabel.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return bounds.size
+    }
+    
+}
